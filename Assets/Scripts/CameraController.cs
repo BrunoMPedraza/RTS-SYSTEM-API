82ebf27878e03934a8f1cbce2924a25773c20961
BrunoMPedraza <61323575+BrunoMPedraza@users.noreply.github.com>
2021-08-13 17:23:54 -0300

    Lock handler
    Just need to not ignore the Y axis and its done

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,43 +3,92 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public PlayerMotor player;
     public bool keyboardControl;
-    public float panSpeed = 20f;
+    public float cameraSpeed = 5f;
+    public float panSpeed = 5f;
     public float panBorderThickness = 10f;
+    public Vector3 panPosition;
     public Vector2 panLimit;
     public float scrollSpeed = 20f;
-    public float minY = 5f;
+    public float minY = 0;
     public float maxY = 75f;
-    public Vector3 offsetFromTarget;
+
+    #region CameraLock stuff
+    public Transform lockTarget;
+    public Vector3 offSet;
+    public bool isLocked = true;
+    #endregion
+
 
     private void Awake()
     {
+        panPosition = transform.position - offSet;
+        offSet.z *= -1;
         keyboardControl = PlayerInputManager.currentKeyHandler.keyboardControlsCamera;
     }
 
     void Update()
     {
+        #region cameraHold
+        if(Input.GetKeyDown(PlayerInputManager.GetKeyCode("camera_lockHold_key"))){
+            isLocked = !isLocked;
+        }
+        if(isLocked){
+            panPosition = lockTarget.position;
+            LookAtPosition(lockTarget.position);
+        }
+        else{
+            #endregion
 
-        if(Input.GetKeyDown(PlayerInputManager.GetKeyCode("camera_lockOnce_key")) && player)
-            transform.position = player.transform.position + offsetFromTarget;
-        var pos = transform.position;
-        pos.z += 
+        panPosition.z += 
         (Input.mousePosition.y >= Screen.height - 
         panBorderThickness ? 1 : Input.mousePosition.y <= 
         panBorderThickness ? -1 : 0 + (keyboardControl ? Input.GetAxisRaw("Vertical") : 0) * panSpeed * Time.deltaTime);
 
-        pos.x += 
+        panPosition.x += 
         (Input.mousePosition.x >= Screen.width - 
         panBorderThickness ? 1 : Input.mousePosition.x <= 
         panBorderThickness ? -1 : 0 + (keyboardControl ? Input.GetAxisRaw("Horizontal") : 0) * panSpeed * Time.deltaTime);
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        pos.y -= scroll * scrollSpeed * 1000f * Time.deltaTime;
+        panPosition.y -= scroll * scrollSpeed * 1000f * Time.deltaTime;
 
-        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
-        transform.position = pos;
+        panPosition.x = Mathf.Clamp(panPosition.x, -panLimit.x, panLimit.x);
+        panPosition.y = Mathf.Clamp(panPosition.y, minY, maxY);
+        panPosition.z = Mathf.Clamp(panPosition.z, -panLimit.y, panLimit.y);
+        LookAtPosition(panPosition);
+        }
+    }
+
+    public void LookAtPosition(Vector3 target)
+    {
+        transform.position = Vector3.Lerp(transform.position, CalculateDesiredPosition(target), cameraSpeed * Time.deltaTime);
+    }
+
+    public Vector3 CalculateDesiredPosition(Vector3 target){ //CALLBACK
+        return new Vector3(target.x, target.y + offSet.y, target.z - offSet.z);;
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(panPosition, Vector3.one);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(new Vector3(transform.position.x, panPosition.y, transform.position.z), panPosition);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(new Vector3(transform.position.x, panPosition.y, transform.position.z), transform.position);
+
+        if(lockTarget)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(lockTarget.position, lockTarget.localScale);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(new Vector3(transform.position.x, lockTarget.position.y, transform.position.z), lockTarget.position);
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(new Vector3(transform.position.x, lockTarget.position.y, transform.position.z), transform.position);
+        }
     }
 }
