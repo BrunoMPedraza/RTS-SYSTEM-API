698ae0b74a804180c94e25ed5ac1fc14ffde4c37
BrunoMPedraza <61323575+BrunoMPedraza@users.noreply.github.com>
2021-08-14 16:47:14 -0300

    Zoom limits
    Now you zoom is limited to the number given to minY and maxY, also you can now zoom in and out on focus.
    Next: Restore zoom to default.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,14 +10,16 @@ public class CameraController : MonoBehaviour
     public Vector3 panPosition;
     public Vector2 panLimit;
     public float scrollSpeed = 20f;
-    public float minY = 0;
-    public float maxY = 75f;
+    public float minY = -20f;
+    public float maxY = 45f;
+    public float defaultZoom = 19.6f;
+    public bool rememberZoom = false;
+    public bool allowZoom = true;
 
     #region CameraLock stuff
     public Transform lockTarget;
     public Vector3 offSet;
     public bool isLocked = true;
-    public bool rememberZoom = true;
     #endregion
 
 
@@ -26,10 +28,36 @@ public class CameraController : MonoBehaviour
         panPosition = transform.position - offSet;
         offSet.z *= -1;
         keyboardControl = PlayerInputManager.currentKeyHandler.keyboardControlsCamera;
+        // panPosition.y = defaultZoom;
     }
 
     void Update()
     {
+        #region zoomLimits
+            
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (rememberZoom && allowZoom)
+        { //IF YES THEN IT WILL REMEMBER THE ACTUAL ZOOM, DOESNT WORK IN EXECUTION
+            if (panPosition.y < minY)
+            panPosition.y = minY;
+            else if (panPosition.y > maxY)
+            panPosition.y = maxY;
+            else
+            panPosition.y -= scroll * scrollSpeed * 1000f * Time.deltaTime;
+        }
+        else if (allowZoom)
+        { //DOESNT WORK IN EXECUTION!!
+            
+            if (offSet.y <  Math.Abs(minY))
+            offSet.y =  Math.Abs(minY);
+            else if (offSet.y > maxY+maxY)
+            offSet.y = maxY+maxY;
+            else
+            offSet.y -= scroll * scrollSpeed * 1000f * Time.deltaTime;
+        }
+        #endregion
+
+
         #region cameraHold
         if(Input.GetKeyDown(PlayerInputManager.GetKeyCode("camera_lockHold_key"))){
             isLocked = !isLocked;
@@ -38,7 +66,11 @@ public class CameraController : MonoBehaviour
             offSet.z = offSet.y/2; // RELATIVE OFFSET BASED UPON ZOOM ON CAMERA
             panPosition = lockTarget.position;
             LookAtPosition(lockTarget.position);
-        }
+        
+      
+        
+            
+        } //ZOOM SHOULD BE INSIDE ISLOCKED
         else{
             #endregion
 
@@ -52,12 +84,7 @@ public class CameraController : MonoBehaviour
         panBorderThickness ? 1 : Input.mousePosition.x <= 
         panBorderThickness ? -1 : 0 + (keyboardControl ? Input.GetAxisRaw("Horizontal") : 0) * panSpeed * Time.deltaTime);
 
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if(rememberZoom) //IF YES THEN IT WILL REMEMBER THE ACTUAL ZOOM, DOESNT WORK IN EXECUTION
-        offSet.y -= scroll * scrollSpeed * 1000f * Time.deltaTime;
-        else
-        panPosition.y -= scroll * scrollSpeed * 1000f * Time.deltaTime;
-
+ 
         panPosition.x = Mathf.Clamp(panPosition.x, -panLimit.x, panLimit.x);
         panPosition.y = Mathf.Clamp(panPosition.y, minY, maxY);
         panPosition.z = Mathf.Clamp(panPosition.z, -panLimit.y, panLimit.y);
@@ -96,4 +123,7 @@ public class CameraController : MonoBehaviour
             Gizmos.DrawLine(new Vector3(transform.position.x, lockTarget.position.y, transform.position.z), transform.position);
         }
     }
+
+
+
 }
