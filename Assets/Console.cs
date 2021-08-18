26f0960204d157a731a4dbafe697b162b337719e
BrunoMPedraza <61323575+BrunoMPedraza@users.noreply.github.com>
2021-08-18 11:41:28 -0300

    Text instantiate
    Using static functions

--- Assets/Console.cs
+++ Assets/Console.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class Console : MonoBehaviour
 {
     private GameObject _panelConsole;
+    private static GameObject _textConsole;
     private bool _pause;
+    public static GameObject textPrefab;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        _textConsole = transform.GetChild(1).gameObject;
         _panelConsole = transform.GetChild(0).gameObject;
+        textPrefab = Resources.Load<GameObject>("UI/ConsoleLog");
         _panelConsole.SetActive(false);
         _pause = false;
     }
@@ -22,12 +30,25 @@ public class Console : MonoBehaviour
     {
         if(Input.GetKeyDown(PlayerInputManager.GetKeyCode("console_switchConsole"))){
             _panelConsole.SetActive(!_panelConsole.activeSelf);
+            
         }
+
+    }
+    public static void Log(string message){
+        GameObject lastCreatedMessage = Instantiate(textPrefab, _textConsole.transform);
+        DateTime _currentTime = DateTime.Now; //this is a leak
+        lastCreatedMessage.GetComponent<TMP_Text>().SetText("> <i> ["+_currentTime+"] </i>"+message);
+    }
+
+
+    void HandleText(){
 
     }
 
     public void HandleFreezeGame(){
         _pause = !_pause;
         Time.timeScale = _pause ? 0 : 1;
+        Log(_pause ? "Paused" : "Unpaused");
+        
     }
 }
